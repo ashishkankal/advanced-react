f6f57631c2908bcbea261e17621e25f3e26498ae
Ashish Kankal <akankal@netchexonline.com>
2019-02-06 18:46:57 +0530

    Added axios with request

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,17 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   }
 };
